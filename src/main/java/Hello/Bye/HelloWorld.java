fe38d8a195fa1904a627882cb30d145b21054777
wayne497 <wayne497wayne497@gmail.com>
2022-01-18 11:06:56 +0800

    init

---
+++ src/main/java/Hello/Bye/HelloWorld.java
@@ -0,0 +1,25 @@
+package Hello.Bye;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+	public int Add(int x,int y)
+	{
+		return x+y;
+	}
+	public int Sub(int x,int y)
+	{
+		return x-y;
+	}
+	public int Mul(int x,int y)
+	{
+		return x*y;
+	}
+	public int Div(int x,int y)
+	{
+		return x/y;
+	}
+}
---
+++ src/test/java/Hello/Bye/testHelloWorld.java
@@ -0,0 +1,14 @@
+package Hello.Bye;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testHelloWorld {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
